dd503b411c9e3df0fb269ecae662d55c15a10916
ayushnilabh <ayush.n@bizfab.com>
2024-05-30 11:48:31 +0530

    type added

--- api/controllers/eventsController/get_all_events.js
+++ api/controllers/eventsController/get_all_events.js
@@ -2,10 +2,18 @@ const events = require('../../models/eventsSchema');
 
 const get_All_events =  async (req, res) => {
     try {
-        const Events = await events.find().sort({ start_at: -1 });;
+    //     console.log('Received query parameters:', req.query); 
+         const query = {};
+    //    console.log("query",query);
+        if (req.query.type) {
+            query.type = req.query.type;
+        }
+        const Events = await events.find(query).sort({ start_at: 1 });;
         res.status(200).json(Events);
     } catch (err) {
+        console.log(err.message);
         res.status(500).json({ message: 'Server error' });
     }
-}
+};
 module.exports = { get_All_events }; 
+
