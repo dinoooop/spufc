0278a542787e1b8e424990c47c4973430d937db0
Dinoop <dinoop.k@bizfab.com>
2024-06-03 10:12:45 +0530

    minor update

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const api = process.env.REACT_APP_API || 'http://127.0.0.1:8800/api';
-const uploads = 'http://127.0.0.1:8800/uploads';
+const uploads = process.env.REACT_APP_UPLOADS || 'http://127.0.0.1:8800/uploads';
 
 
 const config = {
